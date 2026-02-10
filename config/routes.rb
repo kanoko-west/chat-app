071952e65b9c3cfc5610451d2cc0286503de0ee6
kanoko-west <kanoko.kn2508@gmail.com>
2026-02-10 12:35:50 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  root to: "messages#index"
-  #編集しました
   devise_for :users
+  root to: "messages#index"
+  resources :users, only: [:edit, :update]
+
 end
